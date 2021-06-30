1bbc7fb20bd3d852fe97a9d6acacc5cedf66e1c6
Sumit Khandelwal <sumitgniit@gmail.com>
2021-06-30 18:34:54 +0530

    functions added

--- 01-basics/index.js
+++ 01-basics/index.js
@@ -57,7 +57,166 @@
 // console.log(x===y);          // Value & type-checking checking
 
 
-// FUNCTIONS - not just a syntax, can be treated as a value
+// FUNCTIONS 
+// - not just a syntax, can be treated as a value
+// - Can be sent as parameter - Higher Order Functions (HOF)
+// - Functions can be nested, returned
+// - IIFE - Immediately Invoked Function Expressions - self-invoked
+    // - Functions create scope of variables
+    // - Avoids global pollution
+
+
+    // Lexical Scoping - it matters where the function is created
+
+    // var x = 200;
+    // // Case 01
+    // function b (){
+    //     console.log(x);         // 200
+    // }
+
+    // function a(){
+    //     var x = 100;
+    //     // Case 02
+    //     // function b (){
+    //     //     console.log(x);         // 100
+    //     // }
+    //     b();
+    // }
+
+    // a();
+
+
+    // Scope chaining
+
+    // var x = 100;
+    // function a(){
+    //     var x = 200;
+    //     function b(){
+    //         var x = 300;
+    //         console.log(x);         // 300
+    //     }
+    //     b();
+    // }
+
+    // a();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // (function(){
+//     console.log("IIFE Called..")
+//     var x = 100;
+// })();
+
+// console.log(x);
+
+
+
+
+
+
+// Creational - chooseNumber-> 7 -> Overriden by chooseNumber -> 12
+// Executional - return chooseNumber;
+
+// Function Hoisting
+// function mystery(){
+//     function chooseNumber(){
+//         return 7
+//     }
+//     return chooseNumber;
+//     function chooseNumber(){
+//         return 12
+//     }
+// }
+
+// var nestedFunc = mystery()
+// console.log(nestedFunc());          // ?
+
+
+// - Creation chooseNumber - 'undefined'
+// - Executional chooseNumber = function(){} -> return chooseNumber;
+
+// function mystery(){
+//     var chooseNumber = function (){
+//         return 7
+//     }
+//     return chooseNumber;
+//     var chooseNumber = function (){
+//         return 12
+//     }
+// }
+
+// var nestedFunc = mystery()
+// console.log(nestedFunc());  // 7
+
+// function Syntax (Hot Spots) vs. function Expression
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// - Error first, Callback last
+
+// function greet(err, username){
+//     if(err){
+//         console.log(err);
+//         process.exit(1);
+//     }else{
+//         console.log("Hello " + username);
+//     }
+// }
+
+// function greetWithName( name, arr, cb){
+//     if(arr.length > 2){
+//         cb(new Error("Too many elements"));
+//     }else{
+//         cb(null, name);
+//     }
+// }
+
+// greetWithName( "John", [1,2,3,4], greet);            // ?
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Function Syntax
 // function add(num1,num2){
 //     return num1 + num2;
@@ -65,9 +224,9 @@
 // console.log(add(4,5));
 
 // Function Expression
-var add = function(num1, num2){
-    return num1 + num2
-}
+// var add = function(num1, num2){
+//     return num1 + num2
+// }
 
-console.log(add(2,3));       // ?
+// console.log(add(2,3));       // ?
 
